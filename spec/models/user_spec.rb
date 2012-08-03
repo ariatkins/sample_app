8edb5a77f9e5285ff46fc5fc4e5ed766c3132aa4
Ari Atkins <ari@unpluggedinstruments.com>
2012-08-03 12:19:28 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
 
   it { should be_valid }
 
@@ -118,6 +119,11 @@ describe User do
   		@user.save
   		@user.reload.email.should == mixed_case_email.downcase
   	end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 end
